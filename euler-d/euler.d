cd1469c540579daea32e98f1538d01355d1d7b57
Tobias Mueller <toby.mueller.87@gmail.com>
2015-07-18 20:50:52 -0400

    Initial version

---
+++ euler-d/euler.d
@@ -0,0 +1,34 @@
+﻿module main;
+
+import std.stdio,
+	std.algorithm,
+	std.range,
+	std.conv,
+	std.string,
+	std.array,
+	std.conv,
+	std.typecons,
+	toolbox;
+
+auto problem1()
+{
+	return iota(1000)
+		.filter!(x => !(x % 3) || !(x % 5))
+		.sum;
+}
+
+auto problem2()
+{
+	return fibonacci(1L, 2L)
+		.takeWhile!(x => x < 4_000_000)
+		.filter!(x => !(x % 2))
+		.sum;
+}
+
+void main(string[] args)
+{
+	problem2.writeln;
+	
+	// Lets the user press <Return> before program returns
+	stdin.readln();
+}
---
+++ euler-d/toolbox.d
@@ -0,0 +1,191 @@
+﻿module toolbox;
+
+import std.algorithm,
+	std.typecons,
+	std.traits,
+	std.range,
+	std.array,
+	std.conv;
+
+auto apply(alias f, T...)(T t)
+	if(is(typeof(f(t))))
+{
+	static if(is(typeof(f(t)) == void))
+	{
+		f(t);
+		return 0;
+	}
+	else
+	{
+		return f(t);
+	}
+}
+
+auto bind(alias f, T)(T t)
+	if(isTuple!T)
+{
+	return t.expand
+		.apply!f;
+}
+
+auto flip(alias f, T, U...)(U u, T t)
+	if(is(typeof(f(t, u))))
+{
+	return fuse(t, u).expand
+		.apply!f;
+}
+
+auto fuse(T, U)(T t, U u)
+{
+	static if(isTuple!T)
+	{
+		static if(isTuple!U)
+		{
+			return tuple(t.expand, u.expand);
+		}
+		else
+		{
+			return tuple(t.expand, u);
+		}
+	}
+	else
+	{
+		static if(isTuple!U)
+		{
+			return tuple(t, u.expand);
+		}
+		else
+		{
+			return tuple(t, u);
+		}
+	}
+}
+
+template RangeDim(T)
+{
+	static if(isInputRange!T)
+	{
+		alias E = ElementType!T;
+		immutable RangeDim = 1 + RangeDim!E;
+	}
+	else
+	{
+		immutable RangeDim = 0;
+	}
+}
+
+template RElementType(R)
+{
+	static if(RangeDim!R > 0)
+	{
+		alias E = ElementType!R;
+		alias RElementType = RElementType!E;
+	}
+	else
+	{
+		alias RElementType = R;
+	}
+}
+
+auto rreduce(alias f, R)(R r)
+	if(RangeDim!R > 0)
+{
+	static if(RangeDim!R > 1)
+	{
+		return r.map!(e => e.rreduce!f)
+			.reduce!f;
+	}
+	else
+	{
+		return r.reduce!f;
+	}
+}
+
+auto rmap(alias f, R)(R r)
+	if(RangeDim!R > 0)
+{
+	static if(RangeDim!R > 1)
+	{
+		return r.map!(e => e.rmap!f);
+	}
+	else
+	{
+		return r.map!f;
+	}
+}
+
+auto rfilter(alias f, R)(R r)
+	if(RangeDim!R > 0)
+{
+	static if(RangeDim!R > 1)
+	{
+		return r.map!(e => e.rfilter!f)
+			.filter!(e => !e.empty);
+	}
+	else
+	{
+		return r.filter!f;
+	}
+}
+
+auto tmap(R)(R r)
+	if((RangeDim!R == 1) && isTuple!(ElementType!R))
+{
+	return r.map!(t => t.bind!f);
+}
+
+auto tfilter(R)(R r)
+	if((RangeDim!R == 1) && isTuple!(ElementType!R))
+{
+	return r.filter!(t => t.bind!f);
+}
+
+auto trmap(alias f, R)(R r)
+	if(RangeDim!R > 0)
+{
+	static if(RangeDim!R > 1)
+	{
+		return r.map!(e => e.trmap!f);
+	}
+	else
+	{
+		return r.tmap!f;
+	}
+}
+
+auto trfilter(alias f, R)(R r)
+	if(RangeDim!R > 0)
+{
+	static if(RangeDim!R > 1)
+	{
+		return r.map!(e => e.trfilter!f)
+			.filter!(e => !e.empty);
+	}
+	else
+	{
+		return r.tfilter!f;
+	}
+}
+
+auto fibonacci(T)(T first, T second)
+{
+	return tuple(first, second)
+		.expand
+		.recurrence!((a, n) => a[n - 2] + a[n - 1]);
+}
+
+auto sum(R)(R r){	return r.reduce!((acc, x) => acc + x); }
+
+auto takeWhile(alias f, R)(R r)
+	if((RangeDim!R == 1) &&
+	   is(typeof(f(r.front)) == bool))
+{
+	return r.take(r.countUntil!(e => !f(e)));
+}
+
+auto dropWhile(alias f, R)(R r)
+	if((RangeDim!R == 1) &&
+	   is(typeof(f(r.front)) == bool))
+{
+	return r.drop(r.countUntil!(e => !f(e)));
+}
