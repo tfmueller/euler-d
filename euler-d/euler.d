27acc5af77e6365c32d4848981e0dc3bb66e302b
Tobias Mueller <toby.mueller.87@gmail.com>
2015-07-24 22:02:03 -0400

    Changed tabs to spaces.

--- euler-d/euler.d
+++ euler-d/euler.d
@@ -1,71 +1,71 @@
 ﻿module main;
 
 import std.stdio,
-	std.algorithm,
-	std.range,
-	std.conv,
-	std.typecons,
-	toolbox;
+    std.algorithm,
+    std.range,
+    std.conv,
+    std.typecons,
+    toolbox;
 
 auto problem1()
 {
-	return iota(1000)
-		.filter!(x => !(x % 3) || !(x % 5))
-		.sum;
+    return iota(1000)
+        .filter!(x => !(x % 3) || !(x % 5))
+        .sum;
 }
 
 auto problem2()
 {
-	return fibonacci(1, 2)
-		.takeWhile!(x => x < 4_000_000)
-		.filter!(x => !(x % 2))
-		.sum;
+    return fibonacci(1, 2)
+        .takeWhile!(x => x < 4_000_000)
+        .filter!(x => !(x % 2))
+        .sum;
 }
 
 auto problem3()
 {
-	return 600851475143UL
-		.primefacs[$ - 1];
+    return 600851475143UL
+        .primefacs[$ - 1];
 }
 
 auto problem4()
 {
-	return iota(101U, 1000U)
-		.map!(x => iota(100U, x)
-			.map!(y => (x * y)))
-		.rfilter!ispalindrome
-		.rmax;
+    return iota(101U, 1000U)
+        .map!(x => iota(100U, x)
+            .map!(y => (x * y)))
+        .rfilter!ispalindrome
+        .rmax;
 }
 
 auto problem5()
 {
-	return iota(20U, 0U, -1)
-		.reduce!((acc, x) => lcm(acc, x));
+    return iota(20U, 0U, -1)
+        .reduce!((acc, x) => lcm(acc, x));
 }
 
 auto problem6()
 {
-	return tuple(1, 100)
-		.bind!((x, y) => tuple(
-			iota(x, y + 1)
-				.map!(n => n ^^ 2)
-				.sum,
-			iota(x, y + 1)
-				.sum
-				.apply!(n => n ^^ 2))
-			.bind!((sumofsquares, squareofsums) =>
-				squareofsums - sumofsquares));
+    return tuple(1, 100)
+        .bind!((x, y) => tuple(
+            iota(x, y + 1)
+                .map!(n => n ^^ 2)
+                .sum,
+            iota(x, y + 1)
+                .sum
+                .apply!(n => n ^^ 2))
+            .bind!((sumofsquares, squareofsums) =>
+                squareofsums - sumofsquares));
 }
 
 auto testproblem()
 {
-	return [[1, 2, 3], [4, 5, 6], [7, 8, 9, 10]].rsum;
+    return [[1, 2, 3], [4, 5, 6], [7, 8, 9, 10]].rsum;
 }
 
 void main(string[] args)
 {
-	problem6.writeln;
-	
-	// Lets the user press <Return> before program returns
-	stdin.readln();
+    problem6.writeln;
+    
+    // Lets the user press <Return> before program returns
+    stdin.readln();
 }
--- euler-d/toolbox.d
+++ euler-d/toolbox.d
@@ -1,243 +1,243 @@
 ﻿module toolbox;
 
 import std.algorithm,
-	std.typecons,
-	std.traits,
-	std.range,
-	std.array,
-	std.conv,
-	std.math,
-	std.numeric,
-	std.stdio;
+    std.typecons,
+    std.traits,
+    std.range,
+    std.array,
+    std.conv,
+    std.math,
+    std.numeric,
+    std.stdio;
 
 auto apply(alias f, T...)(T t)
-	if(is(typeof(f(t))))
+    if(is(typeof(f(t))))
 {
-	static if(is(typeof(f(t)) == void))
-	{
-		f(t);
-		return 0;
-	}
-	else
-	{
-		return f(t);
-	}
+    static if(is(typeof(f(t)) == void))
+    {
+        f(t);
+        return 0;
+    }
+    else
+    {
+        return f(t);
+    }
 }
 
 auto bind(alias f, T)(T t)
-	if(isTuple!T)
+    if(isTuple!T)
 {
-	return t.expand
-		.apply!f;
+    return t.expand
+        .apply!f;
 }
 
 auto flip(alias f, T, U...)(U u, T t)
-	if(is(typeof(f(t, u))))
+    if(is(typeof(f(t, u))))
 {
-	return fuse(t, u).expand
-		.apply!f;
+    return fuse(t, u).expand
+        .apply!f;
 }
 
 auto fuse(T, U)(T t, U u)
 {
-	static if(isTuple!T)
-	{
-		static if(isTuple!U)
-		{
-			return tuple(t.expand, u.expand);
-		}
-		else
-		{
-			return tuple(t.expand, u);
-		}
-	}
-	else
-	{
-		static if(isTuple!U)
-		{
-			return tuple(t, u.expand);
-		}
-		else
-		{
-			return tuple(t, u);
-		}
-	}
+    static if(isTuple!T)
+    {
+        static if(isTuple!U)
+        {
+            return tuple(t.expand, u.expand);
+        }
+        else
+        {
+            return tuple(t.expand, u);
+        }
+    }
+    else
+    {
+        static if(isTuple!U)
+        {
+            return tuple(t, u.expand);
+        }
+        else
+        {
+            return tuple(t, u);
+        }
+    }
 }
 
 template RangeDim(T)
 {
-	static if(isInputRange!T)
-	{
-		alias E = ElementType!T;
-		immutable RangeDim = 1 + RangeDim!E;
-	}
-	else
-	{
-		immutable RangeDim = 0;
-	}
+    static if(isInputRange!T)
+    {
+        alias E = ElementType!T;
+        immutable RangeDim = 1 + RangeDim!E;
+    }
+    else
+    {
+        immutable RangeDim = 0;
+    }
 }
 
 template RElementType(R)
 {
-	static if(RangeDim!R > 0)
-	{
-		alias E = ElementType!R;
-		alias RElementType = RElementType!E;
-	}
-	else
-	{
-		alias RElementType = R;
-	}
+    static if(RangeDim!R > 0)
+    {
+        alias E = ElementType!R;
+        alias RElementType = RElementType!E;
+    }
+    else
+    {
+        alias RElementType = R;
+    }
 }
 
 auto rreduce(alias f, R)(R r)
-	if(RangeDim!R > 0)
+    if(RangeDim!R > 0)
 {
-	static if(RangeDim!R > 1)
-	{
-		return r.map!(e => e.rreduce!f)
-			.reduce!f;
-	}
-	else
-	{
-		return r.reduce!f;
-	}
+    static if(RangeDim!R > 1)
+    {
+        return r.map!(e => e.rreduce!f)
+            .reduce!f;
+    }
+    else
+    {
+        return r.reduce!f;
+    }
 }
 
 auto rmap(alias f, R)(R r)
-	if(RangeDim!R > 0)
+    if(RangeDim!R > 0)
 {
-	static if(RangeDim!R > 1)
-	{
-		return r.map!(e => e.rmap!f);
-	}
-	else
-	{
-		return r.map!f;
-	}
+    static if(RangeDim!R > 1)
+    {
+        return r.map!(e => e.rmap!f);
+    }
+    else
+    {
+        return r.map!f;
+    }
 }
 
 auto rfilter(alias f, R)(R r)
-	if(RangeDim!R > 0)
+    if(RangeDim!R > 0)
 {
-	static if(RangeDim!R > 1)
-	{
-		return r.map!(e => e.rfilter!f)
-			.filter!(e => !e.empty);
-	}
-	else
-	{
-		return r.filter!f;
-	}
+    static if(RangeDim!R > 1)
+    {
+        return r.map!(e => e.rfilter!f)
+            .filter!(e => !e.empty);
+    }
+    else
+    {
+        return r.filter!f;
+    }
 }
 
 auto tmap(R)(R r)
-	if((RangeDim!R == 1) && isTuple!(ElementType!R))
+    if((RangeDim!R == 1) && isTuple!(ElementType!R))
 {
-	return r.map!(t => t.bind!f);
+    return r.map!(t => t.bind!f);
 }
 
 auto tfilter(R)(R r)
-	if((RangeDim!R == 1) && isTuple!(ElementType!R))
+    if((RangeDim!R == 1) && isTuple!(ElementType!R))
 {
-	return r.filter!(t => t.bind!f);
+    return r.filter!(t => t.bind!f);
 }
 
 auto trmap(alias f, R)(R r)
-	if(RangeDim!R > 0)
+    if(RangeDim!R > 0)
 {
-	static if(RangeDim!R > 1)
-	{
-		return r.map!(e => e.trmap!f);
-	}
-	else
-	{
-		return r.tmap!f;
-	}
+    static if(RangeDim!R > 1)
+    {
+        return r.map!(e => e.trmap!f);
+    }
+    else
+    {
+        return r.tmap!f;
+    }
 }
 
 auto trfilter(alias f, R)(R r)
-	if(RangeDim!R > 0)
+    if(RangeDim!R > 0)
 {
-	static if(RangeDim!R > 1)
-	{
-		return r.map!(e => e.trfilter!f)
-			.filter!(e => !e.empty);
-	}
-	else
-	{
-		return r.tfilter!f;
-	}
+    static if(RangeDim!R > 1)
+    {
+        return r.map!(e => e.trfilter!f)
+            .filter!(e => !e.empty);
+    }
+    else
+    {
+        return r.tfilter!f;
+    }
 }
 
 auto fibonacci(T)(T first, T second)
 {
-	return tuple(first, second)
-		.expand
-		.recurrence!((a, n) => a[n - 2] + a[n - 1]);
+    return tuple(first, second)
+        .expand
+        .recurrence!((a, n) => a[n - 2] + a[n - 1]);
 }
 
-auto prod(R)(R r){	return r.reduce!((acc, x) => acc * x); }
-auto max(R)(R r){	return r.reduce!((acc, x) => acc > x ? acc : x); }
-auto min(R)(R r){	return r.reduce!((acc, x) => acc < x ? acc : x); }
+auto prod(R)(R r){    return r.reduce!((acc, x) => acc * x); }
+auto max(R)(R r){    return r.reduce!((acc, x) => acc > x ? acc : x); }
+auto min(R)(R r){    return r.reduce!((acc, x) => acc < x ? acc : x); }
 
-auto rsum(R)(R r){	return r.rreduce!((acc, x) => acc + x); }
-auto rprod(R)(R r){	return r.rreduce!((acc, x) => acc * x); }
-auto rmax(R)(R r){	return r.rreduce!((acc, x) => acc > x ? acc : x); }
-auto rmin(R)(R r){	return r.rreduce!((acc, x) => acc < x ? acc : x); }
+auto rsum(R)(R r){    return r.rreduce!((acc, x) => acc + x); }
+auto rprod(R)(R r){    return r.rreduce!((acc, x) => acc * x); }
+auto rmax(R)(R r){    return r.rreduce!((acc, x) => acc > x ? acc : x); }
+auto rmin(R)(R r){    return r.rreduce!((acc, x) => acc < x ? acc : x); }
 
 auto takeWhile(alias f, R)(R r)
-	if((RangeDim!R == 1) &&
-	   is(typeof(f(r.front)) == bool))
+    if((RangeDim!R == 1) &&
+       is(typeof(f(r.front)) == bool))
 {
-	return r.take(r.countUntil!(e => !f(e)));
+    return r.take(r.countUntil!(e => !f(e)));
 }
 
 auto dropWhile(alias f, R)(R r)
-	if((RangeDim!R == 1) &&
-	   is(typeof(f(r.front)) == bool))
+    if((RangeDim!R == 1) &&
+       is(typeof(f(r.front)) == bool))
 {
-	return r.drop(r.countUntil!(e => !f(e)));
+    return r.drop(r.countUntil!(e => !f(e)));
 }
 
 auto isqrt(T)(T num)
-	if(isIntegral!T)
+    if(isIntegral!T)
 {
-	return num.to!real
-		.sqrt
-		.to!T;
+    return num.to!real
+        .sqrt
+        .to!T;
 }
 
 T[] primefacs(T)(T num)
-	if(isUnsigned!T)
+    if(isUnsigned!T)
 {
-	if(num == 1)
-	{
-		return T[].init;
-	}
-	if(num == 2)
-	{
-		return [2.to!T];
-	}
-	
-	return num.isqrt
-		.apply!(ns => iota(2.to!T, ns)
-			.chain(num.only)
-			.filter!(x => !(num % x))
-			.front
-			.apply!(x => [x] ~ (num / x).primefacs));
+    if(num == 1)
+    {
+        return T[].init;
+    }
+    if(num == 2)
+    {
+        return [2.to!T];
+    }
+    
+    return num.isqrt
+        .apply!(ns => iota(2.to!T, ns)
+            .chain(num.only)
+            .filter!(x => !(num % x))
+            .front
+            .apply!(x => [x] ~ (num / x).primefacs));
 }
 
 auto lcm(T)(T x, T y)
-	if(isIntegral!T)
+    if(isIntegral!T)
 {
-	return x * y / gcd(x, y);
+    return x * y / gcd(x, y);
 }
 
 bool ispalindrome(T)(T num)
-	if(isUnsigned!T)
+    if(isUnsigned!T)
 {
-	return num.to!string
-		.apply!(ns => ns.retro
-			.equal(ns));
+    return num.to!string
+        .apply!(ns => ns.retro
+            .equal(ns));
 }
